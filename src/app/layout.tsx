9eb4ba2b43306b2dea6122005bca474848836271
Eddie Sanmarco <eddie@Eddies-MacBook-Pro.local>
2022-12-05 06:43:38 -0700

    updated nextjs to use app dir

---
+++ src/app/layout.tsx
@@ -0,0 +1,12 @@
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" data-theme="dark">
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+}
