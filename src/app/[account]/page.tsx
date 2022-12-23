d3a26b37a42afbeb09e1245ff34b86328e63b2c0
Eddie Sanmarco <ehsanmarco@gmail.com>
2022-12-23 08:38:34 -0700

    deleted test account page

--- src/app/[account]/page.tsx
+++
@@ -1,7 +0,0 @@
-export default async function Account({ params }: { params: { account: string } }) {
-    console.log(params.account, 'account ID');
-
-    const accountFetching = await (await fetch('...')).json();
-
-    return null;
-}
